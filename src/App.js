55baa9dcc307b67a4e9435bcafbffe18c1df8683
DeveshTanwar123 <88023668+DeveshTanwar123@users.noreply.github.com>
2023-05-03 10:14:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+import HomeContainer from './Components/HomeContainer';
+export default function App() {
+  return (
+    <div>
+      <HomeContainer />
+    </div>
+  );
+}
